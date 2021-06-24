a88e50d734a3944477acd353ef64c230cbc0ac0f
rakib3004 <bsse1129@iit.du.ac.bd>
2021-06-24 16:58:36 +0600

    add guest and append new guest in the list in HotelGuest.py

--- IntroductionOSAutomation/network.py
+++ IntroductionOSAutomation/network.py
@@ -5,13 +5,15 @@ import socket
 
 def check_localhost():
     localhost = socket.gethostbyname('localhost')
+    print(localhost)
     if localhost == '127.0.0.1':
         return True
 
     return False
 def check_connectivity():
-    request = requests.get("https://www.google.com")
+    request = requests.get("http://www.google.com")
     responses = request.status_code
+    print(responses)
     if responses == 200:
         return True
 
---
+++ ManagingFiles/HotelGuest.py
@@ -0,0 +1,24 @@
+guests = open("guests.txt", "w")
+initial_guests = ["Bob", "Andrea", "Manuel", "Polly", "Khalid"]
+
+for i in initial_guests:
+    guests.write(i + "\n")
+
+guests.close()
+
+with open("guests.txt") as guests:
+    for line in guests:
+        print(line)
+
+new_guests = ["Sam", "Danielle", "Jacob"]
+
+with open("guests.txt", "a") as guests:
+    for i in new_guests:
+        guests.write(i + "\n")
+
+guests.close()
+
+with open("guests.txt") as guests:
+    for line in guests:
+        print(line)
+
