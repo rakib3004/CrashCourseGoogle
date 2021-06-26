e41aae9049036dcf8bfbacc028a108dd85b02a93
rakib3004 <bsse1129@iit.du.ac.bd>
2021-06-26 14:07:07 +0600

    convert te4xt into Cloud Image like CloudImage.png in CloudImageFromText.py

---
+++ FinalProject/CloudImageFromText.py
@@ -0,0 +1,48 @@
+# This is the uploader widget
+
+def _upload():
+
+    _upload_widget = list()
+
+    def _cb(change):
+        global file_contents
+        decoded = 'some-thing'
+        #io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+
+    _upload_widget.observe(_cb, names='data')
+    #display(_upload_widget)
+
+_upload()
+
+
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+                           "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its",
+                           "they", "them", \
+                           "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be",
+                           "been", "being", \
+                           "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when",
+                           "where", "how", \
+                           "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very",
+                           "can", "will", "just"]
+    count = 0
+    # LEARNER CODE START HER
+
+    wordCount = {}
+    file_contents = file_contents.split()
+    data = ""
+
+    for word in file_contents:
+        data = "".join(ch for ch in word if ch.isalnum())
+        checkData = data.lower()
+        if checkData not in uninteresting_words:
+            if checkData in wordCount:
+                wordCount[checkData] += 1
+            else:
+                wordCount[checkData] = 1
