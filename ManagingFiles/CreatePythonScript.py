579796d2467313089283e684d6e4204db5725446
rakib3004 <bsse1129@iit.du.ac.bd>
2021-06-27 15:18:47 +0600

    write script to create a python file in CreatePythonScript.py

---
+++ ManagingFiles/CreatePythonScript.py
@@ -0,0 +1,8 @@
+def create_python_script(filename):
+  comments =  "# Start of a new Python program"
+  with open(filename, "w") as file:
+    file.write(comments)
+    filesize = len(comments)
+  return(filesize)
+
+print(create_python_script("program.py"))
